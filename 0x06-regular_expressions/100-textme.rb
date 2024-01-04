7143e618a7e3c1e7a24e661bc7900b6f63cc31e3
Danielyilma <yilmad752@gmail.com>
2024-01-04 23:28:48 +0300

    regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-result = ARGV[0].scan(/from:([+\w]*).*to:([+0-9]*).*flags:([(-1|0):]+)/)
-puts result[0][0] + ", " + result[0][1] + ", " + result[0][2]
+puts ARGV[0].scan(/(?<=from:|to:|flags:)[^\]]*/).join(',')
