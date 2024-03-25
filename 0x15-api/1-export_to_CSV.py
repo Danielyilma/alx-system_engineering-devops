f9566124d9909d9e3115b8f4a695314a58602bc0
Danielyilma <yilmad752@gmail.com>
2024-03-26 00:32:52 +0300

    api usage

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,41 +1,30 @@
 #!/usr/bin/python3
-"""
-The function retrieves and displays the completed tasks
-of a specific employee from a JSON API.
-"""
-from sys import argv
+"""exporting api from third party to a csv file"""
 import csv
 import requests
+from sys import argv
 
 
-def main():
-    '''getting data for an api'''
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(
-        argv[1]
-    )
-    url2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-        argv[1]
-    )
-    response = requests.get(url)
-    name = response.json().get('username', None)
-    csv_dict = []
-    response = requests.get(url2)
-
-    for task in response.json():
-        dic = {}
-        dic['userId'] = task.get('userId')
-        dic['username'] = name
-        dic['completed'] = task.get('completed')
-        dic['title'] = task.get('title')
-        csv_dict.append(dic)
-
-    fields = ['userId', 'username', 'completed', 'title']
-    filename = argv[1] + '.csv'
-
-    with open(filename, 'w', encoding='utf-8') as f:
-        write = csv.DictWriter(f, fieldnames=fields, quoting=csv.QUOTE_ALL)
-        write.writerows(csv_dict)
-
-
+"""
+The code block starting with `if __name__ == '__main__':`
+is the main entry point of the script. It is executed when
+the script is run directly (not imported as a module).
+"""
 if __name__ == '__main__':
-    main()
+    url = F"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    to_do = "/todos"
+    name = ""
+    with requests.get(url) as response:
+        name = response.json()['username']
+    url = url + to_do
+    task_completed = 0
+    with requests.get(url) as response:
+        response = response.json()
+        for i in response:
+            if i['completed'] is True:
+                task_completed += 1
+        with open(str(argv[1]) + ".csv", "w", encoding="utf-8") as csv_file:
+            csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+            for i in response:
+                line = [argv[1], name, i['completed'], i['title']]
+                csv_writer.writerow(line)
