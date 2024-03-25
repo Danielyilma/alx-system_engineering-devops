a31a75a112aeb4b64f1f0adf5f051722a9e1e0b5
Danielyilma <yilmad752@gmail.com>
2024-03-25 16:47:54 +0300

    api usage

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,22 +3,29 @@
 import requests
 from sys import argv
 
+
 url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
 url2 = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos'
 totalTask = 0
 taskCompleted = 0
 
-response = requests.get(url)
-name = response.json().get('name', None)
 
-response = requests.get(url2)
+def main():
+    response = requests.get(url)
+    name = response.json().get('name', None)
 
-for task in response.json():
-    if task.get('completed'):
-        taskCompleted += 1
-    totalTask += 1
+    response = requests.get(url2)
 
-print(f'Employee {name} is done with tasks({taskCompleted}/{totalTask}):')
-for task in response.json():
-    if task.get('completed'):
-        print(f'\t {task.get("title")}')
+    for task in response.json():
+        if task.get('completed'):
+            taskCompleted += 1
+        totalTask += 1
+
+    print(f'Employee {name} is done with tasks({taskCompleted}/{totalTask}):')
+    for task in response.json():
+        if task.get('completed'):
+            print(f'\t {task.get("title")}')
+
+
+if __name__ == '__main__':
+    main()
