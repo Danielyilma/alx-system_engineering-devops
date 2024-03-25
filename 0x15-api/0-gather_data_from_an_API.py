3f4140561a222bb004d3443215b02c9c0041ccec
Danielyilma <yilmad752@gmail.com>
2024-03-25 17:00:02 +0300

    api usage

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,12 +4,10 @@ import requests
 from sys import argv
 
 
-url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
-url2 = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos'
-
-
 def main():
     '''getting data for an api'''
+    url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
+    url2 = f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos'
     totalTask = 0
     taskCompleted = 0
     response = requests.get(url)
