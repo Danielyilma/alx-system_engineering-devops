3728fe15aa5d69116264a84322819e73118b1311
Danielyilma <yilmad752@gmail.com>
2024-04-19 23:43:48 +0300

    subscribers count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,8 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     '''finding the number of subscribers for a valid subreddit'''
-    response = requests.get(f'https://reddit.com/r/{subreddit}/about.json',
-                            allow_redirects=False)
+    response = requests.get(f'https://reddit.com/r/{subreddit}/about.json')
 
     if response.status_code != 200:
         return 0
