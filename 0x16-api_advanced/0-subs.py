d83619b0293237096b081adec6bfb6e3d2cedd95
Danielyilma <yilmad752@gmail.com>
2024-04-19 21:42:04 +0300

    subscribers count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,9 @@ import requests
 def number_of_subscribers(subreddit):
     response = requests.get('https://reddit.com/subreddits/search.json', params={'q':subreddit})
 
-    if response.json().get('data').get('children') == []:
+    if response.json().get('data', {}).get('children') == []:
         return 0
 
     response = requests.get(f'https://reddit.com/r/{subreddit}/about.json')
 
-    return response.json().get('data').get('subscribers')
+    return response.json().get('data', {}).get('subscribers')
