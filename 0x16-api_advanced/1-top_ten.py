4409eaec76dc3b8dfc15a5af8a9e6c97b8e200d7
Danielyilma <yilmad752@gmail.com>
2024-05-07 20:15:24 +0300

    top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,13 +10,13 @@ def top_ten(subreddit):
     """prints the titles of the top ten hot posts for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         print(None)
-    r = requests.get('http://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit))
-    
+    r = requests.get(
+        'http://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
+    )
+
     if r.status_code == 200:
         r = r.json().get('data', None)
         for i in r.get('children', None):
             print(i.get("data").get('title', None))
     else:
-        print(None)    
-    
-
+        print(None)
